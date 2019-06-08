535199317e3e27b1ba8bccbe1b1957dcc0e064d2
dcuomo <dcuomo@outlook.com>
2019-06-08 15:05:30 -0700

    Testing auto Data version 2

--- tests/unit/unit.tests.ps1
+++ tests/unit/unit.tests.ps1
@@ -1,6 +1,11 @@
 $DataFile   = Import-PowerShellDataFile .\$($env:repoName).psd1 -ErrorAction SilentlyContinue
 $TestModule = Test-ModuleManifest       .\$($env:repoName).psd1 -ErrorAction SilentlyContinue
 
+write-output $env:build
+write-output $env:build
+write-output $env:build
+write-output $env:build
+
 Describe "$($env:repoName)-Manifest" {
     Context Validation {
         It "[Import-PowerShellDataFile] - $($env:repoName).psd1 is a valid PowerShell Data File" {
