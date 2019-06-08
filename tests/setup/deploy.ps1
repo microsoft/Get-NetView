ee252ac39d6aad0a2de24aa07b54ac43700a0b88
Omar Cardona <ocardona@microsoft.com>
2019-06-08 15:37:18 -0700

    NIT braces

--- tests/setup/deploy.ps1
+++ tests/setup/deploy.ps1
@@ -12,20 +12,16 @@ Write-Host -Object ''
 
 # Make sure we're using the Master branch and that it's not a pull request
 # Environmental Variables Guide: https://www.appveyor.com/docs/environment-variables/
-if ($env:APPVEYOR_REPO_BRANCH -ne 'master')
-{
+if ($env:APPVEYOR_REPO_BRANCH -ne 'master') {
     Write-Warning -Message "Skipping version increment and publish for branch $env:APPVEYOR_REPO_BRANCH"
 }
-elseif ($env:APPVEYOR_PULL_REQUEST_NUMBER -gt 0)
-{
+elseif ($env:APPVEYOR_PULL_REQUEST_NUMBER -gt 0) {
     Write-Warning -Message "Skipping version increment and publish for pull request #$env:APPVEYOR_PULL_REQUEST_NUMBER"
 }
-else
-{
+else {
     # We're going to add 1 to the revision value since a new commit has been merged to Master
     # This means that the major / minor / build values will be consistent across GitHub and the Gallery
-    Try
-    {
+    Try     {
         # This is where the module manifest lives
         $manifestPath = ".\$($env:RepoName).psd1"
 
@@ -51,14 +47,12 @@ else
         #(Get-Content -Path $manifestPath) -replace 'FunctionsToExport = ', 'FunctionsToExport = @(' | Set-Content -Path $manifestPath -Force
         #(Get-Content -Path $manifestPath) -replace "$($functionList[-1])'", "$($functionList[-1])')" | Set-Content -Path $manifestPath -Force
     }
-    catch
-    {
+    catch     {
         throw $_
     }
 
     # Publish the new version to the PowerShell Gallery
-    Try
-    {
+    Try {
         # Build a splat containing the required details and make sure to Stop for errors which will trigger the catch
         $PM = @{
             Path        = '.'
@@ -70,15 +64,13 @@ else
         Publish-Module @PM
         Write-Host "$($env:RepoName) PowerShell Module version $newVersion published to the PowerShell Gallery." -ForegroundColor Cyan
     }
-    Catch
-    {
+    Catch {
         Write-Warning "Publishing update $newVersion to the PowerShell Gallery failed."
         throw $_
     }
 
     # Publish the new version back to Master on GitHub 
-    Try
-    {
+    Try {
         # Set up a path to the git.exe cmd, import posh-git to give us control over git, and then push changes to GitHub
         # Note that "update version" is included in the appveyor.yml file's "skip a build" regex to avoid a loop
         $env:Path += ";$env:ProgramFiles\Git\cmd"
@@ -90,8 +82,7 @@ else
         git push origin master -q
         Write-Host "$($env:RepoName) PowerShell Module version $newVersion published to GitHub." -ForegroundColor Cyan
     }
-    Catch
-    {
+    Catch {
         Write-Warning "Publishing update $newVersion to GitHub failed."
         throw $_
     }
