68bd46b5aa8a52aeee41cf4d76f9ba4f4f8361f9
Tom Molenhouse <thmolenh@microsoft.com>
2019-06-10 18:44:08 -0700

    Use AppVeyor build version for module manifest

--- tests/setup/deploy.ps1
+++ tests/setup/deploy.ps1
@@ -14,22 +14,19 @@ Write-Host -Object ''
 # Environmental Variables Guide: https://www.appveyor.com/docs/environment-variables/
 if ($env:APPVEYOR_REPO_BRANCH -ne 'master') {
     Write-Warning -Message "Skipping version increment and publish for branch $env:APPVEYOR_REPO_BRANCH"
-}
-elseif ($env:APPVEYOR_PULL_REQUEST_NUMBER -gt 0) {
+} elseif ($env:APPVEYOR_PULL_REQUEST_NUMBER -gt 0) {
     Write-Warning -Message "Skipping version increment and publish for pull request #$env:APPVEYOR_PULL_REQUEST_NUMBER"
-}
-else {
-    # We're going to add 1 to the revision value since a new commit has been merged to Master
-    # This means that the major / minor / build values will be consistent across GitHub and the Gallery
-    Try     {
+} else {
+    try {
         # This is where the module manifest lives
         $manifestPath = ".\$($env:RepoName).psd1"
 
         # Start by importing the manifest to determine the version, then add 1 to the revision
         $manifest = Test-ModuleManifest -Path $manifestPath -ErrorAction SilentlyContinue
-        [System.Version]$version = $manifest.Version
-        Write-Output "Old Version: $version"
-        [String]$newVersion = New-Object -TypeName System.Version -ArgumentList ($version.Major, $version.Minor, $version.Build, $env:APPVEYOR_BUILD_NUMBER)
+        $oldVersion = $manifest.Version
+        $newVersion = [Version]::new($env:APPVEYOR_BUILD_VERSION -split "/.") # expected format "yyyy.MM.dd.{build}"
+
+        Write-Output "Old Version: $oldVersion"
         Write-Output "New Version: $newVersion"
 
         # Update the manifest with the new version value and fix the weird string replace bug
@@ -46,13 +43,12 @@ else {
         (Get-Content -Path $manifestPath) -replace 'NewManifest', "$($env:RepoName)" | Set-Content -Path $manifestPath
         #(Get-Content -Path $manifestPath) -replace 'FunctionsToExport = ', 'FunctionsToExport = @(' | Set-Content -Path $manifestPath -Force
         #(Get-Content -Path $manifestPath) -replace "$($functionList[-1])'", "$($functionList[-1])')" | Set-Content -Path $manifestPath -Force
-    }
-    catch     {
+    } catch {
         throw $_
     }
 
     # Publish the new version to the PowerShell Gallery
-    Try {
+    try {
         # Build a splat containing the required details and make sure to Stop for errors which will trigger the catch
         $PM = @{
             Path        = '.'
@@ -63,14 +59,13 @@ else {
 
         Publish-Module @PM
         Write-Host "$($env:RepoName) PowerShell Module version $newVersion published to the PowerShell Gallery." -ForegroundColor Cyan
-    }
-    Catch {
+    } catch {
         Write-Warning "Publishing update $newVersion to the PowerShell Gallery failed."
         throw $_
     }
 
     # Publish the new version back to Master on GitHub 
-    Try {
+    try {
         # Set up a path to the git.exe cmd, import posh-git to give us control over git, and then push changes to GitHub
         # Note that "update version" is included in the appveyor.yml file's "skip a build" regex to avoid a loop
         $env:Path += ";$env:ProgramFiles\Git\cmd"
@@ -81,8 +76,7 @@ else {
         git commit -s -m "Update version to $newVersion"
         git push origin master -q
         Write-Host "$($env:RepoName) PowerShell Module version $newVersion published to GitHub." -ForegroundColor Cyan
-    }
-    Catch {
+    } catch {
         Write-Warning "Publishing update $newVersion to GitHub failed."
         throw $_
     }
