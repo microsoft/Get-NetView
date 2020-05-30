f4017f0d77b4ef6d78db99eedf603b84ea202f6d
Tom Molenhouse <thmolenh@microsoft.com>
2020-05-30 02:00:40 -0700

    attempt #3

--- tests/unit/unit.tests.ps1
+++ tests/unit/unit.tests.ps1
@@ -1,20 +1,18 @@
-$DataFile   = Import-PowerShellDataFile .\$($env:repoName).psd1 -ErrorAction SilentlyContinue
-$TestModule = Test-ModuleManifest       .\$($env:repoName).psd1 -ErrorAction SilentlyContinue
-
 Describe "$($env:repoName)-Manifest" {
     Context Validation {
         It "[Import-PowerShellDataFile] - $($env:repoName).psd1 is a valid PowerShell Data File" {
+            $DataFile = Import-PowerShellDataFile .\$($env:repoName).psd1
             $DataFile | Should -Not -BeNullOrEmpty
         }
 
         It "[Test-ModuleManifest] - $($env:repoName).psd1 should pass the basic test" {
+            $TestModule = Test-ModuleManifest .\$($env:repoName).psd1
             $TestModule | Should -Not -BeNullOrEmpty
         }
 
-        Import-Module .\$($env:repoName).psd1 -ErrorAction SilentlyContinue
-        $command = Get-Command $($env:repoName) -ErrorAction SilentlyContinue
-
         It "Should have the $($env:repoName) function available" {
+            Import-Module .\$($env:repoName).psd1
+            $command = Get-Command $($env:repoName)
             $command | Should -Not -BeNullOrEmpty
         }
     }
